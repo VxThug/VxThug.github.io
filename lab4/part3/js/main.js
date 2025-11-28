c9d72a900d1906f2d3232f07aa4ccf14c8360247
Thug <thujiplayz@gmail.com>
2025-11-28 18:14:13 -0500

    Update main.js

--- lab4/part3/js/main.js
+++ lab4/part3/js/main.js
@@ -29,3 +29,14 @@ class Ball {
     this.size = size;
   }
 }
+
+// Drawing The Ball
+class Ball {
+  // …
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
