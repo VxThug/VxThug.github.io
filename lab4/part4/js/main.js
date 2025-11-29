1ff4e7179039814cb18c4a903725f893b7b33708
Thug <thujiplayz@gmail.com>
2025-11-28 19:05:23 -0500

    Update main.js

--- lab4/part4/js/main.js
+++ lab4/part4/js/main.js
@@ -165,7 +165,7 @@ const balls = [];
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-
+    
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
